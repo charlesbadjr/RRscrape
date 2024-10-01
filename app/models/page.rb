639998d92b285e15248ef170d45a0e779479264e
Charlie Jergins <39018811+charlesbadjr@users.noreply.github.com>
2024-09-30 22:40:54 -0600

    success :results != success:

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,19 +4,19 @@ class Page < ApplicationRecord
 validates :url, presence: true
 validates :check_type, presence: true
 validates :selector, presence: true
-validates :match_text, presence: {if: :check_type?}
+validates :match_text, presence: {if: ->{ check_type == "text" }}
 
 
   def run_check!
     scraper = Scraper.new(url)
     result = case check_type
-          when "text"
-             scraper.text(selector: selector).downcase == match_text.downcase
-          when "exists"
-             scraper.present?
-          when "not_exists"
-             !scraper.present?
-          end
-    results.create(success :result)
+             when "text"
+                scraper.text(selector: selector).downcase == match_text.downcase
+             when "exists"
+                scraper.present?(selector: selector)
+             when "not_exists"
+                !scraper.present?(selector: selector)
+             end
+    results.create(success: result)
   end
 end
